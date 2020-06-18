9d9ed34ce23fc098bcad989ba3bea8baa43b51d4
李文龙 <liwenlong@himango.cn>
2020-06-18 09:37:31 +0800

    !

--- app/Models/Content/Resources.php
+++ app/Models/Content/Resources.php
@@ -10,7 +10,7 @@ class Resources extends Model
 {
     public $table = 'content_resources';
 
-
+    protected $fillable = ['content_id'];
     public $timestamps = true;
     const CREATED_AT = 'create_time';
     const UPDATED_AT = 'update_time';
