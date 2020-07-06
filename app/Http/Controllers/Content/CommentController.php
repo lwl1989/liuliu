7ddd5afb7664c540d1af63976fc1a1e7d8d1b37d
李文龙 <liwenlong@himango.cn>
2020-07-06 16:33:55 +0800

    fixed

--- app/Http/Controllers/Content/CommentController.php
+++ app/Http/Controllers/Content/CommentController.php
@@ -114,7 +114,7 @@ class CommentController extends Controller
             $zanCount = UserZan::query()->where('typ', 2)
                 ->where('obj_id', $commentIds)
                 ->groupBy('obj_id')
-                ->selectRaw('count(*) as count, obj_id')->toArray();
+                ->selectRaw('count(*) as count, obj_id')->get()->toArray();
             foreach ($comments as &$comment) {
                 $comment['zanCount'] = 0;
                 foreach ($zanCount as $item) {
